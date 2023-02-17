0b8556bd3b4c7f537f53063c5739f30914724e77
Mayubha <artisticmayuba@gmail.com>
2023-02-16 23:47:51 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - entry
- * Return: 1
+ * main - c program that prints a line to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of at is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
