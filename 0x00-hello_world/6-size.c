90c88e60c246f5f59a93c2f23d2d71b8ac002993
Mayubha <artisticmayuba@gmail.com>
2023-02-16 22:52:47 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,6 @@
  */
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
-	long longType;
-	long long longlongType;
-
 	printf("size of a char: %zu byte(s)\n", sizeof(charType));
 	printf("size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("size of a long int: %zu byte(s)\n", sizeof(longType));
