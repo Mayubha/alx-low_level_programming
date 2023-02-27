14adc490f3d4485e7d13e58422198bd57ffb2bb2
Mayubha <artisticmayuba@gmail.com>
2023-02-27 02:50:14 -0800

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string to get the length of
+ * Return: tue length of @str.
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while (*str++)
+		length++;
+
+	return (length);
+}
