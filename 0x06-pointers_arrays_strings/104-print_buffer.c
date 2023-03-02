4f978069753696dffb336fd3dc33f8dcecafc679
Mayubha <artisticmayuba@gmail.com>
2023-03-02 11:59:44 -0800

    buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,41 +11,42 @@
  */
 void print_buffer(char *b, int size)
 {
-	int o, j, i;
+	int byte, index;
 
-	o = 0;
+	for (byte = 0; byte < size; byte += 10)
+	{
+		printf("%08x: ", byte);
 
-	if (size <= 0)
-	{
-		printf("\n");
-		return;
-	}
-	while (o <  size)
-	{
-		j = size - 0 < 10 ? size - o : 10;
-		printf("%08x: ", o);
-		for (i = 0; i < 10; i++)
+		for (index = 0; index < 10; index++)
 		{
-			if (i < j)
-				printf("%02x", *(b + o + i));
-			else
+			if ((index + byte) >= size)
 				printf("  ");
-			if (i % 2)
-			{
-				printf(" ");
-			}
-		}
-		for (i = 0; i < j; i++)
-		{
-			int c = *(b + o + i);
 
-			if (c >= 31 || c <= 126)
-			{
-				c = '.';
-			}
-			printf("%c", c);
+			else
+				printf("%02x", *(b + index + byte));
+
+			if ((index % 2) != 0 && index != 0)
+				printf(" ");
 		}
+
+		for (index = 0; index < 10; index++)
+		{
+			if ((index + byte) >= size)
+				break;
+
+			else if (*(b + index + byte) >= 31 &&
+				*(b + index + byte) <= 126)
+				printf("%c", *(b + index + byte));
+
+			else
+				printf(".");
+		}
+
+		if (byte >= size)
+			continue;
+
 		printf("\n");
-		o += 10;
 	}
+	if (size <= 0)
+		printf("\n");
 }
