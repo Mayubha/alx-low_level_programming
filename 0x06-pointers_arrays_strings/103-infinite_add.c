3d076a76c7ab80b21f8e4f9f0840fc084961bf0d
Mayubha <artisticmayuba@gmail.com>
2023-03-02 09:13:25 -0800

    infinite add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -32,7 +32,7 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		tens = num / 10;
 	}
 
-	for (; *n2; n2--; r_index--)
+	for (; *n2; n2--, r_index--)
 	{
 		num = (*n2 - '0') + tens;
 		*(r + r_index) = (num % 10) + '0';
@@ -48,33 +48,4 @@ char *add_strings(char *n1, char *n2, char *r, int r_index)
 		return (0);
 
 	return (r + r_index + 1);
-}
-/**
- * infinite_add - adds two numbers.
- * @n1: the first number to be added.
- * @n2: the second number to be added.
- * @r: the buffer to store the result
- * @size_r: the buffer size.
- *
- * Return: if r can store the sum - a pointer to the result.
- * if r cannot store the sum - 0
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int index, n1_len = 0, n2_len = 0;
-
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
-
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
-
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-		return (0);
-
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
 }
