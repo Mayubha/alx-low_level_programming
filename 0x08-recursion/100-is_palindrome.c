3d90b0febbfa0d3e7be9857cbfd429121db0d3aa
Mayubha <artisticmayuba@gmail.com>
2023-03-08 11:49:24 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,57 +1,62 @@
 #include "main.h"
 
-int check_palindrome(char *s);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
 /**
- * is_palindrome - returns if a sring is palindrome
- * @s: the string valued to be checked
+ * find_strlen - returns length of a string
+ * @s: the string to be measured
  *
- * Return: integer value
+ * Return: the length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - checks string if palindrome
+ * @s: string to be checked
+ * @len: the length of s
+ * @index: index of the string to be checked
+ *
+ * Return: if the string is a palindrome - 1
+ * if the string is not palindrome - 0
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks palindrome
+ * @s: string to check
+ *
+ * Return: if string is palindrome - 1
+ * if not - 0
  */
 int is_palindrome(char *s)
 {
-	if (*s == '0')
+	int index = 0;
+	int len = find_strlen(s);
+
+	if (!(*s))
 		return (1);
-
-	return (check_palindrome(s));
+	return (check_palindrome(s, len, index));
 }
 
-/**
- * check_palindrome - checks if a string is a palindrome
- * @s: the string value to be checked
- *
- * Return: integer value
- */
-int check_palindrome(char *s)
-{
-	int l = _strlen_recursion(s) - 1;
-
-	if (*s == s[l])
-	{
-		s++;
-		l--;
-	}
-	else
-	{
-		return (0);
-	}
-
-	return (1);
-}
-
-/**
- * _strlen_recursion - get the length of a string
- * @s: the string to get the length
- *
- * Return: the string length
- */
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
-
-	s++;
-	return (_strlen_recursion(s) + 1);
-}
