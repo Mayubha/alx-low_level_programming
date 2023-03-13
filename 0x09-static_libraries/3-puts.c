caaae3e929db73939aee8ba7f1c8b832bd04396a
Mayubha <artisticmayuba@gmail.com>
2023-03-13 07:49:53 -0700

    static libraries tasks

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str);
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a mutlilingual puzzle");
+	return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of int
+ *@n: the int to check
+ * Return: absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
